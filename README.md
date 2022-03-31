fbc6fca066fa9dd65e4cfdbfda1196f752c68c6d
Isiah Yates <isyates@outlook.com>
2022-03-30 22:28:37 -0700

    updates readme, removes needless comments from JS

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ City Life comparison
 
 A site that allows you to compare two cities across 17 different metrics including Healthcare, housing availability, Startup scene and cost of living. USes the open API from https://developers.teleport.org/
 
-This project uses HTML5, CSS,  and Javascript with jQuery (get version)
+This project uses HTML5, CSS,  and Javascript with jQuery
 
-
+Usage:
+Select two cities from the dropdown list and select the compare cities button.
+This will then give you a select of the metrics for each city and compares the values, highlighting the greater of the two values, or highlighting them both if they are equal.
 
 Link to the deployed app: https://isyates.github.io/City-Life-Comparison/
--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// objective is to compare two cities across all city indexes from the teleport API protocols and give you a comparison(either using arrows or percentages) of the two cities
-
-// things we will need
-// ability to call two separate inputs into two separate buttons
-// pull the data for each city and list the information in a set of lists by numbers(using LIs in the HTML maybe for static parts maybe?)
-
-// do a comparison of the data and an evaluation to determine if it is greater than or less than for each category.
-
-// extra: build a few example people and rate the cities based on those criteria primarily
-
-// comments from yanny - keep it simple, commit often, make your git commit messages meaningful, when asking questions reach out to peers, help channels then instructors/Yanny
-
 
 const $buttonList1 = $('#buttonSearch1')
 var bigList1
@@ -95,13 +83,6 @@ function CompareCity(entry1, entry2, name1, name2) {
 
 
         
-        // if (entry1[index].score_out_of_10 > entry2[index].score_out_of_10) {
-        //     console.log(`${entry1[index].name} availability is greater in ${name1} than ${name2}`)
-
-        // } else {
-        //     console.log(`${entry1[index].name} availability is greater in ${name2} than ${name1}`)
-        // }
-
 
 
 
@@ -122,12 +103,3 @@ $("#cityImage2").attr('src', data.photos[0].image.web)
 })
 
 }
-
-// now have two working lists and the beginning of the city comparison working. also got 1 button to trigger the entire search which makes more sense
-// city comparison will need to be able to confirm using a loop(?) each potion
-// next steps: still need to try and work out how to get the full city entries without manually entering them all.
-// need to figure out jquery/CSS to get character changes or maybe images displayed for showing the comparisons
-
-// questions:
-// how will i be able to get the data
-// need to rethink the steps that this program will take
